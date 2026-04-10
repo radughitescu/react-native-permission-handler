4cca13eca4e087afd5d0f7d4bd3855c40d2ed793
Radu Ghitescu <radu.ghitescu@metaminds.com>
2026-04-10 13:08:27 +0300

    feat: wire up barrel exports for all public API

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 export type {
-  PermissionFlowState,
-  PermissionFlowEvent,
-  PrePromptConfig,
   BlockedPromptConfig,
-  PermissionCallbacks,
-  PermissionHandlerConfig,
-  PermissionHandlerResult,
   MultiPermissionEntry,
   MultiplePermissionsConfig,
   MultiplePermissionsResult,
+  PermissionCallbacks,
+  PermissionFlowEvent,
+  PermissionFlowState,
+  PermissionHandlerConfig,
+  PermissionHandlerResult,
+  PrePromptConfig,
 } from "./types";
+
+export { transition } from "./core/state-machine";
+export { usePermissionHandler } from "./hooks/use-permission-handler";
+export { useMultiplePermissions } from "./hooks/use-multiple-permissions";
+export { PermissionGate } from "./components/permission-gate";
+export type { PermissionGateProps } from "./components/permission-gate";
+export { DefaultPrePrompt } from "./components/default-pre-prompt";
+export type { DefaultPrePromptProps } from "./components/default-pre-prompt";
+export { DefaultBlockedPrompt } from "./components/default-blocked-prompt";
+export type { DefaultBlockedPromptProps } from "./components/default-blocked-prompt";
